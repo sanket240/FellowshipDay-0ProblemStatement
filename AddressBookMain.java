876d693465b48d879048843d34ccc699d4adbe7e
sanket2498 <sanketdulange24@gmail.com>
2020-12-12 08:32:53 +0530

    Added : Add new person from console

--- AddressBookMain.java
+++ AddressBookMain.java
@@ -1,10 +1,24 @@
 package Addressbook;
+import java.util.Scanner;
 public class AddressBookMain {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		 int choice=1;
 		 PersonMethods personmethods=new PersonMethods();
 		 personmethods.CreatePerson();
+		 Scanner s=new Scanner(System.in);
+		 while(choice!=0)
+		 {
+			 System.out.println("1.Add Person\n");
+			 System.out.println("Enter Your Choice");
+	         choice=s.nextInt();
+	         switch (choice) {
+             case 1:
+            	 personmethods.CreatePerson();
+                 break;
+	         }    
+		 }
 	}
 
 }
