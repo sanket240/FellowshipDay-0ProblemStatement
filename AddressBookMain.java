303b93316476f67f6306266ff7dcefa9383e4033
sanket2498 <sanketdulange24@gmail.com>
2020-12-12 09:03:55 +0530

    Added : To add Multiple Persons functionality

--- AddressBookMain.java
+++ AddressBookMain.java
@@ -4,28 +4,39 @@ public class AddressBookMain {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		 int choice=1;
+		 int choice=1,person_count,i;
 		 PersonMethods personmethods=new PersonMethods();
 		 personmethods.CreatePerson();
 		 Scanner s=new Scanner(System.in);
 		 while(choice!=0)
 		 {
-			 System.out.println("1.Add Person\n2.Update");
+			 System.out.println("1.Add Person\n");
 			 System.out.println("Enter Your Choice");
 	         choice=s.nextInt();
 	         switch (choice) {
              case 1:
-            	 personmethods.CreatePerson();
+            	 System.out.println("Enter Number of persons:");
+            	 person_count=s.nextInt();
+            	 for(i=0;i<person_count;i++)
+            	 {
+            		 personmethods.CreatePerson();
+            	 }
                  break;
              case 2:
             	 System.out.println("Enter name to Update");
                  String name=s.next();
                  System.out.println("Enter what to update");
                  String wht=s.next();
-                 personmethods.update(name,wht);
+                 personmethods.UpdatePerson(name,wht);
                  break;
-	         }    
+             case 3:
+            	 System.out.println("Enter name to Delete");
+                 String n=s.next();
+                 personmethods.DeletePerson(n);
+                 break;
+	         }  
 		 }
 	}
 
 }
+
