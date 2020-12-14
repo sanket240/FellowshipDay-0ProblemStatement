80d65830f68a294825658bfe348444e422a5a121
sanket2498 <sanketdulange24@gmail.com>
2020-12-14 09:45:48 +0530

    Added : View By City and View by City

--- Person.java
+++ Person.java
@@ -3,19 +3,11 @@ import java.util.Comparator;
 import java.util.TreeSet;
 
 public class Person {
-        int id;
         String firstName;
         String lastName;
         String city,state;
         int zip;
         String phNo;
-        public int getId() {
-            return id;
-        }
-
-        public void setId(int id) {
-            this.id = id;
-        }
 
         public String getFirstName() {
             return firstName;
@@ -65,24 +57,11 @@ public class Person {
             this.phNo = phNo;
         }
 
-        TreeSet set = new TreeSet(new Comparator() {
-            @Override
-            public int compare(Object o1, Object o2) {
-                Person a1=(Person)o1;
-                Person a2=(Person)o2;
-                if(a1.getFirstName().equalsIgnoreCase(a2.getFirstName())){
-                    return 0;
-                }
-                return 1;
-            }
-        });
         public static Comparator<Person> PerNameComparator = new Comparator<Person>() {
 
             public int compare(Person s1, Person s2) {
                 String PersonName1 = s1.getFirstName().toUpperCase();
                 String PersonName2 = s2.getFirstName().toUpperCase();
-
-
                 return PersonName1.compareTo(PersonName2);
 
             }};
@@ -91,10 +70,7 @@ public class Person {
                 public int compare(Person s1, Person s2) {
                     String CityName1 = s1.getCity().toUpperCase();
                     String CityName2 = s2.getCity().toUpperCase();
-
-
                     return CityName1.compareTo(CityName2);
-
                 }};
                 
                 public static Comparator<Person> PerStateComparator = new Comparator<Person>() {
@@ -102,11 +78,31 @@ public class Person {
                     public int compare(Person s1, Person s2) {
                         String StateName1 = s1.getState().toUpperCase();
                         String StateName2 = s2.getState().toUpperCase();
-
-
                         return StateName1.compareTo(StateName2);
 
                     }};
+                    
+                    public static Comparator<Person> PerZipComparator = new Comparator<Person>() {
+
+                        public int compare(Person s1, Person s2) {
+                            int Zip1 = s1.getZip();
+                            int Zip2 = s2.getZip();
+                            return Zip1 - Zip2;
+                        }};
+                    
+                    
+                    
+                    public String toString() {
+                        return
+                                ", firstName='" + firstName + '\'' +
+                                ", lastName='" + lastName + '\''+
+                                ", city='" + city + '\'' +
+                                ", state='" + state + '\'' +
+                                ", zip=" + zip +
+                                ", phNo='" + phNo + '\''
+
+                                ;
+                    }
                     
 
 
--- PersonMethods.java
+++ PersonMethods.java
@@ -1,76 +1,77 @@
 package Addressbook;
 import java.util.Collections;
+import java.util.Dictionary;
+import java.util.Hashtable;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 import java.util.Iterator;
 
-public class PersonMethods {
-	int id=0;
-	ArrayList<Person> person=new ArrayList<>();
-	public void CreatePerson()
-	{
-	  id++;
-	  
-      Person person1=new Person();
-      person1.id=id;
-      System.out.println("Enter Your First Name");
-      Scanner scanner=new Scanner(System.in);
-      person1.firstName=scanner.nextLine();
-      System.out.println("Enter Your Last Name");
-      person1.lastName=scanner.nextLine();
-      System.out.println("Enter Your city Name");
-      person1.city=scanner.nextLine();
-      System.out.println("Enter Your state Name");
-      person1.state=scanner.nextLine();
-      System.out.println("Enter Your phone Number");
-      person1.phNo=scanner.nextLine();
-      System.out.println("Enter Your zip");
-      person1.zip=scanner.nextInt();
-      System.out.println("Added Successfully");
-      person.add(person1);	
-	}
+public class PersonMethods
+{
+		ArrayList<Person> person=new ArrayList<>();
+		Scanner scanner=new Scanner(System.in);
+		public void CreatePerson()
+		{
+				  Person person1=new Person();
+			      System.out.println("Enter Your First Name");
+			      Scanner scanner=new Scanner(System.in);
+			      person1.firstName=scanner.nextLine();			   
+			      System.out.println("Enter Your Last Name");
+			      person1.lastName=scanner.nextLine();
+			      System.out.println("Enter Your city Name");
+			      person1.city=scanner.nextLine();
+			      System.out.println("Enter Your state Name");
+			      person1.state=scanner.nextLine();
+			      System.out.println("Enter Your phone Number");
+			      person1.phNo=scanner.nextLine();
+			      System.out.println("Enter Your zip");
+			      person1.zip=scanner.nextInt();
+			      System.out.println("Added Successfully");
+			      person.add(person1);
+		}
 	
-	 public void UpdatePerson(String name,String u)
-	 {
-	        int flag=0;
-	        Scanner s=new Scanner(System.in);
-	        for(Person p:person){
-	        if(p.firstName.equals(name))
+		public void UpdatePerson(String name,String u)
+		{
+			int flag=0;
+	        for(Person p:person)
 	        {
-	                flag=1;
+	        	if(p.firstName.equals(name))
+	        	{
+	        		flag=1;
 	                switch (u)
 	                {
 	                    case "city":
 	                        System.out.println("Enter Your city Name");
-	                        p.city=s.nextLine();
+	                        p.city=scanner.nextLine();
 	                        break;
 	                    case "state":
 	                        System.out.println("Enter Your state Name");
-	                        p.state=s.nextLine();
+	                        p.state=scanner.nextLine();
 	                        break;
-	                    case "phNo":
+	                    case "phone":
 	                        System.out.println("Enter Your phone Number");
-	                        p.phNo=s.nextLine();
+	                        p.phNo=scanner.nextLine();
 	                        break;
 	                    case "zip":
 	                        System.out.println("Enter Your zip");
-	                        p.zip=s.nextInt();
+	                        p.zip=scanner.nextInt();
 	                        break;
 	                }
-	          }
-	      }
-	        if(flag==0){
+	        	}
+	        }
+	        if(flag==0)
+	        {
 	            System.out.println("Not Found");
 	        }
-	        else{
+	        else
+	        {
 	            System.out.println("Update Successfully");
 	        }
 	    }
-	 public void DeletePerson(String name)
-	 {
-		   int flag=0;
-	        Scanner s=new Scanner(System.in);
+		public void DeletePerson(String name)
+		{
+	   	   int flag=0;
 	        Iterator<Person> itr=person.iterator();
 	        while(itr.hasNext())
 	        {
@@ -89,15 +90,7 @@ public class PersonMethods {
 	        {
 	            System.out.println("Delete Successfully");
 	        }
-	 }
-
-	    public ArrayList<Person> unique(){
-	        Person a=new Person();
-	        a.set.addAll(person);
-	        ArrayList<Person>arr=new ArrayList<Person>(a.set);
-
-	        return arr;
-	    }
+		}	  
 	    public void sortByName()
 	    {
 	    	Collections.sort(person,Person.PerNameComparator);
@@ -110,37 +103,49 @@ public class PersonMethods {
 	    {
 	    	Collections.sort(person,Person.PerStateComparator);
 	    }
-
+	    public void sortByZip()
+	    {
+	    	Collections.sort(person,Person.PerZipComparator);
+	    }
 	    public void Display(){
 	        for(Person p:person){
 	            System.out.println(p);
 	        }
 	    }
 	    
-	    public void SearchCity()
-	    {
-	    	Scanner input =new Scanner(System.in);
-	    	System.out.println("Enter city:");
-	    	String city_name=input.nextLine();
-	    	for(Person per : person)
-	    	{
-	    		if(city_name.equals(per.city))
-	    		{
-	    			System.out.println(per.toString());
-	    		}
-	    	}
-	    }
-	    public void SearchState()
-	    {
-	    	Scanner input =new Scanner(System.in);
-	    	System.out.println("Enter state:");
-	    	String state_name=input.nextLine();
-	    	for(Person per : person)
-	    	{
-	    		if(state_name.equals(per.state))
-	    		{
-	    			System.out.println(per.toString());
-	    		}
-	    	}
-	    }
+	  
+
+		public void viewByCity()
+		{
+			System.out.println("Enter city name");
+			String city=scanner.next();
+			Dictionary infoCity=new Hashtable();
+			for(Person a:person)
+			{
+				if(a.city.equals(city)) 
+				{
+					infoCity.put(a.firstName, city);
+				}
+				
+			}
+			System.out.println("The Dictionary Contains:"+infoCity);
+			
+		}
+		
+		public void viewByState() 
+		{
+			System.out.println("Enter State name");
+			String state=scanner.next();
+			Dictionary infoState=new Hashtable();
+			for(Person a:person)
+			{
+				if(a.state.equals(state))
+				{
+					infoState.put(a.firstName, state);
+				}
+				
+			}
+			System.out.println("The Dictionary Contains:"+infoState);
+			
+		}
 }
